052dfbe0bcf44e96b0f31feb4322dd8dffda4cda
Qarrale <proger16510@gmail.cpm>
2020-11-21 19:15:30 +0300

    feat: saveDate ready

--- src/main/java/ru/bez_createha/queue_bot/view/HandlersRegistrator.java
+++ src/main/java/ru/bez_createha/queue_bot/view/HandlersRegistrator.java
@@ -3,11 +3,8 @@ package ru.bez_createha.queue_bot.view;
 
 import ru.bez_createha.queue_bot.controller.TelegramController;
 import ru.bez_createha.queue_bot.view.backs.BackGroupView;
-import ru.bez_createha.queue_bot.view.createQueue.CalendarBack;
-import ru.bez_createha.queue_bot.view.createQueue.CalendarForward;
-import ru.bez_createha.queue_bot.view.createQueue.StepOneView;
+import ru.bez_createha.queue_bot.view.createQueue.*;
 import org.springframework.stereotype.Service;
-import ru.bez_createha.queue_bot.view.createQueue.StepTwoView;
 
 @Service
 public class HandlersRegistrator {
@@ -19,8 +16,9 @@ public class HandlersRegistrator {
     private final StepTwoView stepTwoView;
     private final CalendarBack calendarBack;
     private final CalendarForward calendarForward;
+    private final SaveDate saveData;
 
-    public HandlersRegistrator(GroupView groupView, GroupCreated groupCreated, QueueView queueView, BackGroupView backGroupView, StepOneView stepOneView, StepTwoView stepTwoView, CalendarBack calendarBack, CalendarForward calendarForward) {
+    public HandlersRegistrator(GroupView groupView, GroupCreated groupCreated, QueueView queueView, BackGroupView backGroupView, StepOneView stepOneView, StepTwoView stepTwoView, CalendarBack calendarBack, CalendarForward calendarForward, SaveDate saveData) {
         this.groupView = groupView;
         this.stepTwoView = stepTwoView;
         this.groupCreated = groupCreated;
@@ -29,6 +27,7 @@ public class HandlersRegistrator {
         this.stepOneView = stepOneView;
         this.calendarBack = calendarBack;
         this.calendarForward = calendarForward;
+        this.saveData = saveData;
     }
 
     public void registerAllHandlers(TelegramController telegramController){
@@ -40,5 +39,6 @@ public class HandlersRegistrator {
         telegramController.registerCallbackHandler(stepOneView);
         telegramController.registerCallbackHandler(calendarBack);
         telegramController.registerCallbackHandler(calendarForward);
+        telegramController.registerCallbackHandler(saveData);
     }
 }
--- src/main/java/ru/bez_createha/queue_bot/view/createQueue/SaveDate.java
+++ src/main/java/ru/bez_createha/queue_bot/view/createQueue/SaveDate.java
@@ -2,19 +2,30 @@ package ru.bez_createha.queue_bot.view.createQueue;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import ru.bez_createha.queue_bot.context.UserContext;
 import ru.bez_createha.queue_bot.model.State;
 import ru.bez_createha.queue_bot.model.User;
 import ru.bez_createha.queue_bot.view.CallbackCommand;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.function.Predicate;
 
 @Component
-public class SaveData implements CallbackCommand{
+public class SaveDate implements CallbackCommand{
+
+    private final UserContext userContext;
+
+    public SaveDate(UserContext userContext) {
+        this.userContext = userContext;
+    }
+
+
     @Override
     public Predicate<String> statePredicate() {
         return s -> s.equals(State.ENTER_QUEUE_DATE.toString());
@@ -29,20 +40,17 @@ public class SaveData implements CallbackCommand{
     public List<BotApiMethod<? extends Serializable>> process(CallbackQuery callbackQuery, User user) {
 
         List<BotApiMethod<? extends Serializable>> methods = new ArrayList<>();
+        userContext.getUserStaff(user.getUserId()).getRawQueue().setDay_start(Integer.valueOf(callbackQuery.getData().split("::")[1].split("/")[0]));
+        userContext.getUserStaff(user.getUserId()).getRawQueue().setMonth_start(Integer.valueOf(callbackQuery.getData().split("::")[1].split("/")[1]));
+        userContext.getUserStaff(user.getUserId()).getRawQueue().setYear_start(Integer.valueOf(callbackQuery.getData().split("::")[1].split("/")[2]));
+
 
         EditMessageText editMessageText = new EditMessageText();
         editMessageText.setMessageId(user.getMessageId());
         editMessageText.setChatId(callbackQuery.getMessage().getChatId().toString());
-        editMessageText.setText("Вы успешно создали очередь с именем: "+callbackQuery.getData().split("::")[2]+"\nПредпологаемая дата начала: "+callbackQuery.getData().split("::")[1]);
+        editMessageText.setText("Выберите время начала.\n Для этого напишите в чат время в формате Часы:Минуты");
 
-
-
-
-
-
-
-
-
+        methods.add(editMessageText);
         return methods;
     }
 }
