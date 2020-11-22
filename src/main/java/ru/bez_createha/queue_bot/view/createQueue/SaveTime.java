5a71c15243ad710b46675ca2e33c45d69f184937
Qaralle <proger16510@gmail.com>
2020-11-22 06:04:34 +0300

    fix: create fixed

--- src/main/java/ru/bez_createha/queue_bot/view/createQueue/SaveTime.java
+++ src/main/java/ru/bez_createha/queue_bot/view/createQueue/SaveTime.java
@@ -93,6 +93,7 @@ public class SaveTime implements MessageCommand {
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(String.valueOf(queue.getGroupId().getChatId()));
             sendMessage.setText("Очередь "+queue.getTag()+"создана. Начало: "+date.toString());
+            bot.execute(sendMessage);
 //            } catch (SchedulerException e) {
 //                //
 //                editMessageText.setText("Матовый");
